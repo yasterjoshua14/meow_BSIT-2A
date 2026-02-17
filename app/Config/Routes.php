45c03455959ab2bfbdc6ede9c06a75201d421d26
yasterjoshua14 <yasterjoshua@gmail.com>
2026-02-17 13:51:43 +0800

    Person crud

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,29 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+
+$routes->get('/', 'Auth::index');
+$routes->get('/login', 'Auth::index');
+$routes->post('/auth', 'Auth::auth');
+$routes->get('/dashboard', 'Dashboard::index');
+$routes->get('/logout', 'Auth::logout');
+
+// User Acounts routes
+
+$routes->get('/users', 'Users::index');
+$routes->post('users/save', 'Users::save');
+$routes->get('users/edit/(:segment)', 'Users::edit/$1');
+$routes->post('users/update', 'Users::update');
+$routes->delete('users/delete/(:num)', 'Users::delete/$1');
+$routes->post('users/fetchRecords', 'Users::fetchRecords');
+
+// Person routes
+$routes->get('/person', 'Person::index');
+$routes->post('person/save', 'Person::save');
+$routes->get('person/edit/(:segment)', 'Person::edit/$1');
+$routes->post('person/update', 'Person::update');
+$routes->delete('person/delete/(:num)', 'Person::delete/$1');
+$routes->post('person/fetchRecords', 'Person::fetchRecords');
+
+// Logs routes for admin
+$routes->get('/log', 'Logs::log');
